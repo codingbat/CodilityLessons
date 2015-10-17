80071bc342c61cade73cce79baba1b44faa90bc3
codingbat <Nazmul.Alam@students.ittralee.ie>
2015-10-17 22:15:59 +0100

    Tape Equilibrium Solution
    Signed-off-by: codingbat <Nazmul.Alam@students.ittralee.ie>

--- Codility/me/nazmulalam/lesson_one/TapeEquilibrium.py
+++ Codility/me/nazmulalam/lesson_one/TapeEquilibrium.py
@@ -1,5 +1,44 @@
+#!/usr/bin/python
+
 '''
 Created on 17 Oct 2015
 
-@author: Naz
+@author: Nazmul Alam
 '''
+
+class TapeEquilibrium:
+    def __init__(self):
+        self.data = []
+    
+    def solution(self, A):
+        lowest = 0
+        leftSum = 0
+        rightSum = 0
+        for x in range(1, len(A)):
+            leftSum = leftSum + self.leftsum(x, A)
+            rightSum = self.rightsum(x, A)
+            #print leftSum, rightSum 
+            #print x
+            if x == 1:
+                lowest = abs(leftSum - rightSum)
+            if (abs(leftSum-rightSum) < lowest):
+                lowest = abs(leftSum - rightSum)
+            
+        return lowest
+    
+    def leftsum(self, p, A):
+        total = 0
+        for p in range(p-1, p):
+            total = total + abs(A[p])
+        return total
+    
+    def rightsum(self, p, A):
+        total = 0
+        for p in range(p, len(A)):
+            total = total + abs(A[p])
+        return total
+
+obj = TapeEquilibrium()
+A = [3, 1, 2, 4, 3]
+
+print obj.solution(A)
