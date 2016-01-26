88e9b8d88c83471e881530eb6fa46deff0061728
Nazmul Alam <nalam@fexco.com>
2016-01-26 14:02:29 +0000

    Fixed leading zeros in binary numbers

--- Codility/me/nazmulalam/lesson_one/BinaryGap.java
+++ Codility/me/nazmulalam/lesson_one/BinaryGap.java
@@ -1,29 +1,32 @@
 import java.util.List;
 import java.util.ArrayList;
 
-class BinaryGap {
+class Solution {
     public int solution(int N) {
         // write your code in Java SE 8
         List<Integer> binary = new ArrayList();
         
         while (N > 0) {
-            binary.add(N%2);
-            N = N/2;
+            if (N % 2 == 1 || binary.size() > 0) 
+                binary.add(N%2);
+                N = N/2;
         }
         
-        //System.out.print(binary);
-        // int longestGap = 0;
-        // for (int i = 0; i < binary.size()-1; i++)
-        // {
-        //     int gap = 0;
-        //     while (i+1 != 1) {
-        //         gap++;
-        //         i++;
-        //     }
+        // System.out.print(binary);
+        int longestGap = 0;
+        for (int i = 0; i < binary.size()-1; i++)
+        {
+            int gap = 0;
+            if (i == 1) {
+                while (i+1 == 0) {
+                    gap++;
+                    i++;
+                }
+            }
             
-        //     if(gap>longestGap && binary.get(i-1)==1)
-        //         longestGap=gap;
-        // }
+            if(gap>longestGap)
+                longestGap=gap;
+        }
         
         
         
